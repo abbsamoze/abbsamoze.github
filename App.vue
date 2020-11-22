a7450ae11eb766a56c5b1a8f9b0eead0f17a5563
abbsamoze <69853489+abbsamoze@users.noreply.github.com>
2020-11-22 21:43:58 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <ovning></ovning>
+  </div>
+</template>
+
+<script>
+import ovning from './components/ovning.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ovning,
+
+  }
+}
+</script>
+
+<style>
+
+</style>
